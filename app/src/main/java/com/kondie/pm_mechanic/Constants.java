99e413594a8c7ae6c3608b0d3018b03f57761f82
kondie <kondien@bbd.co.za>
2020-02-20 12:02:25 +0200

    ...

--- app/src/main/java/com/kondie/pm_mechanic/Constants.java
+++ app/src/main/java/com/kondie/pm_mechanic/Constants.java
@@ -9,12 +9,12 @@ public class Constants {
     public static final int SUCCESS = 0;
     public static final int FAILURE = 1;
     public static final String PACKAGE_NAME = "com.kondie.pm_mechanic";
-    public static final String GOOGLE_API_KEY = "AIzaSyCU2IHlKut_21MWpe0Nj1ceGTCxGpu6F3c";
+    public static final String GOOGLE_API_KEY = "AIzaSyBwBtDsNntA7GG8yc_A0flfWvqtSCXABQw";
     public static final String RECEIVER = PACKAGE_NAME + ".RECEIVER";
     public static final String RESULT_DATA_KEY = PACKAGE_NAME + ".RESULT_DATA_KEY";
     public static final String LOCATION_DATA_EXTRA = PACKAGE_NAME + ".LOCATION_DATA_EXTRA";
-    public static final String PM_HOSTING_WEBSITE = "https://specials-1523610414581.appspot.com/";
-    public static final String BUCKET_NAME = "pocket_mechanic_bucket";
+    public static final String PM_HOSTING_WEBSITE = "https://pocket-mechanic-268506.appspot.com/";
+    public static final String BUCKET_NAME = "pocket_mechanic_bucket2";
     public static final String WRONG_PART = BUCKET_NAME + ".storage.googleapis.com";
     public static final String CORRECT_PART = "storage.googleapis.com/" + BUCKET_NAME;
 }
